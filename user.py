2d8d95b1804a097b77a899e53e6fb54e63c2cc85
WillUniRal <147512158+WillUniRal@users.noreply.github.com>
2025-02-26 21:16:01 +0000

    gitignore wasnt hidden

--- user.py
+++ user.py
@@ -1,17 +1,26 @@
 from abc import ABC, abstractmethod
 from random import randint
 from datetime import datetime
+import bcrypt
+import uuid
 
 class User(ABC) :
-    def __init__(self,fname,lname,email) :
+    def __init__(self,fname,lname,email,pass_word) :
         self.first_name = fname
         self.last_name = lname
         self.id = randint(100000000,999999999)
         self.email = email
+        self.__pass_word = bcrypt.hashpw(pass_word,bcrypt.hashpw())
         self.join_date = datetime.now()
+
     @property
     def name(self) :
-        return self.first_name + " " +self.last_name
+        return self.first_name+" "+self.last_name
+    
+    def authenticate_pw(self,pw) :
+        session = uuid.uuid4() if bcrypt.checkpw(pw,self.__pass_word) else None
+        return session
+            
     @abstractmethod
     def view_dashboard() :
         pass
