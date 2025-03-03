4129b05e84d5ce35b97f76eabeee08e0cdd610af
WillUniRal <147512158+WillUniRal@users.noreply.github.com>
2025-03-03 22:51:43 +0000

    forgot to save

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from member import Member
 from librarian import Librarian
 from getpass import getpass
+from dataset import Server 
 
 # Client console based for testing structure
 # Has all functional requirements for end user 
 def login() :
     email = input("Please enter your email: ")
     password = getpass()
+    return email, password
 
 if __name__ == "__main__" :
-
-    login()
+    credentials = login()
+    server = Server()
+    try :
+        server.find_user(credentials[0])
+    except :
+        pass
     
